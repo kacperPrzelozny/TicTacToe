7f980dd3b84775e94c9d4efee0d4f4e601527ab9
Kacper Przelozny <przelozny14@gmail.com>
2022-09-12 14:12:11 +0200

    results

--- app/src/main/java/com/kacperprzelozny/tictactoe/ResultActivity.java
+++ app/src/main/java/com/kacperprzelozny/tictactoe/ResultActivity.java
@@ -2,7 +2,11 @@ package com.kacperprzelozny.tictactoe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ResultActivity extends AppCompatActivity {
 
@@ -14,7 +18,30 @@ public class ResultActivity extends AppCompatActivity {
             getSupportActionBar().hide();
         }
         Bundle b = getIntent().getExtras();
-            String result = b.getString("result");
+        String resultText = b.getString("winner");
+        String resultInfoOnView;
+        TextView result = (TextView) findViewById(R.id.resultInfo);
+        if(resultText.equals("cross"))
+            resultInfoOnView = "Wygrywa: krzyżyk";
+        else if(resultText.equals("circle"))
+            resultInfoOnView = "Wygrywa: kółko";
+        else resultInfoOnView = "Remis";
+        result.setText(resultInfoOnView);
 
+        Button backToMenu = (Button) findViewById(R.id.backToMenu);
+        backToMenu.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent switchActivityIntent = new Intent(ResultActivity.this, MainActivity.class);
+                startActivity(switchActivityIntent);
+            }
+        });
+
+        Button playAgain = (Button) findViewById(R.id.playAgain);
+        playAgain.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent switchActivityIntent = new Intent(ResultActivity.this, OfflineActivity.class);
+                startActivity(switchActivityIntent);
+            }
+        });
     }
 }
